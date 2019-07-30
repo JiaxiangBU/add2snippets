a32833fdaad2f1a68f7233a150d0a72c8931932a
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-07-30 21:41:48 +0800

    Add metadata, README and LICENSE; add function `add_uml`.

---
+++ R/add_uml.R
@@ -0,0 +1,44 @@
+#' Paste the workflow figure built with the Unified Modeling Language
+#'
+#' @importFrom glue glue
+#' @importFrom clipr write_clip
+#' @export
+add_uml <- function() {
+  text <- glue::glue('
+  snippet push
+      nomnoml::nomnoml("
+      #stroke: #a86128
+      [<frame>目前点击相关的模型、规则安排|
+        [<abstract>模型v1.0||xxx]
+        [xxx的合作方名单] --> [模型v1.0]
+
+        [<abstract>模型v2.0||xxx模型]
+        [xxx的合作方名单] --> [模型v2.0]
+
+        [<abstract>模型v3.0||user-based xxx]
+        [xxx的合作方名单] --> [模型v3.0]
+
+        [<abstract>模型v3.1||item-based xxx]
+        [xxx的合作方名单] --> [模型v3.1]
+
+        [<abstract>模型v3.2||@xxx 排序]
+        [xxx的合作方名单] --> [模型v3.2]
+
+        [<abstract>默认排序||@xxx]
+        [模型v1.0] --> [默认排序]
+        [模型v2.0] --> [默认排序]
+        [模型v3.0] --> [默认排序]
+        [模型v3.1] --> [默认排序]
+        [模型v3.2] --> [默认排序]
+
+
+        [<abstract>去重逻辑||@xxx]
+        [默认排序] --> [去重逻辑]
+      ]
+      ")')
+  clipr::write_clip(text, allow_non_interactive = TRUE)
+  file.edit("~/.R/snippets/r.snippets")
+  cat("Paste the snippet in the document.")
+  cat("We recommend you do it manually.")
+}
+
--- dev_history_r_pkg.R
+++ dev_history_r_pkg.R
@@ -4,17 +4,13 @@ use_git()
 # https://github.com/JiaxiangBU/add2impala/blob/master/DESCRIPTION
 file.edit("DESCRIPTION")
 library(devtools)
-use_r("add_me")
-use_addin(addin = "add_me")
 
 use_build_ignore("dev_history.R")
 use_roxygen_md()
 use_pipe()
 library(magrittr)
 
-document()
-# load_all()
-install()
+use_readme_rmd()
 rmarkdown::render("README.Rmd")
 file.remove("README.html")
 
@@ -22,6 +18,19 @@ options(usethis.full_name = "Jiaxiang Li")
 use_mit_license()
 
 use_news_md()
+source("../imp_rmd/R/pull_and_push.R")
+
+# add zenodo citation -----------------------------------------------------
+
+library(devtools)
+use_r("add_uml")
+load_all()
+document()
+# load_all()
+install()
+
+# publish -----------------------------------------------------------------
+
 use_version()
 
 # git push
@@ -30,10 +39,4 @@ use_github_release()
 # publish release
 # https://zenodo.org/account/settings/github/
 
-
-# add zenodo citation -----------------------------------------------------
-
-library(devtools)
-use_r("add_zenodo_citation")
-load_all()
-add_zenodo_citation(here::here("../pyks/README.Rmd"))
+add_zenodo_citation(here::here("README.Rmd"))
