7ffb1b5a573854ed05e39227864836bc4f38dd2d
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2020-01-18 21:57:03 +0800

    add imports, add parameters for the functions, fix non-ACSII characters.

--- R/add_uml.R
+++ R/add_uml.R
@@ -2,43 +2,45 @@
 #'
 #' @importFrom glue glue
 #' @importFrom clipr write_clip
+#' @import utils
+#' @param open.r.snippets Logical, by default \code{TRUE}
 #' @export
 add_uml <- function(open.r.snippets = TRUE) {
   text <- glue::glue('
   snippet push
       nomnoml::nomnoml("
       #stroke: #a86128
-      [<frame>目前点击相关的模型、规则安排|
-        [<abstract>模型v1.0||xxx]
-        [xxx的合作方名单] --> [模型v1.0]
+      [<frame>Current_CTR_Model|
+        [<abstract>Model_v1.0||xxx]
+        [Party_A] --> [Model_v1.0]
 
-        [<abstract>模型v2.0||xxx模型]
-        [xxx的合作方名单] --> [模型v2.0]
+        [<abstract>Model_v2.0||xxxModel_]
+        [Party_A] --> [Model_v2.0]
 
-        [<abstract>模型v3.0||user-based xxx]
-        [xxx的合作方名单] --> [模型v3.0]
+        [<abstract>Model_v3.0||user-based xxx]
+        [Party_A] --> [Model_v3.0]
 
-        [<abstract>模型v3.1||item-based xxx]
-        [xxx的合作方名单] --> [模型v3.1]
+        [<abstract>Model_v3.1||item-based xxx]
+        [Party_A] --> [Model_v3.1]
 
-        [<abstract>模型v3.2||@xxx 排序]
-        [xxx的合作方名单] --> [模型v3.2]
+        [<abstract>Model_v3.2||@xxx Rank]
+        [Party_A] --> [Model_v3.2]
 
-        [<abstract>默认排序||@xxx]
-        [模型v1.0] --> [默认排序]
-        [模型v2.0] --> [默认排序]
-        [模型v3.0] --> [默认排序]
-        [模型v3.1] --> [默认排序]
-        [模型v3.2] --> [默认排序]
+        [<abstract>Default_Rank||@xxx]
+        [Model_v1.0] --> [Default_Rank]
+        [Model_v2.0] --> [Default_Rank]
+        [Model_v3.0] --> [Default_Rank]
+        [Model_v3.1] --> [Default_Rank]
+        [Model_v3.2] --> [Default_Rank]
 
 
-        [<abstract>去重逻辑||@xxx]
-        [默认排序] --> [去重逻辑]
+        [<abstract>Distinct_Logic||@xxx]
+        [Default_Rank] --> [Distinct_Logic]
       ]
       ")')
   clipr::write_clip(text, allow_non_interactive = TRUE)
   if(open.r.snippets) {
-      file.edit("~/.R/snippets/r.snippets")
+      utils::file.edit("~/.R/snippets/r.snippets")
   } else {
       print(text)
   }
